bb7971c84902589bee2373837b8794833bd4a718
Rozhin <79437690+Rozhin81@users.noreply.github.com>
2023-05-27 17:36:08 +0330

    Update index.js

--- index.js
+++ index.js
@@ -6,11 +6,9 @@ var http = require('http');
 http.createServer(function (req, res) {
   
     var admin = new mongoose.mongo.Admin(mongoose.connection.db);
-    admin.buildInfo(function (err, info) {
-       console.log(info.version);
-    });
+    var info = await admin.buildInfo();
   
-  res.write('Hello World!'); //write a response to the client
+  res.write('Hello World!' + info.version); //write a response to the client
   res.end(); //end the response
 }).listen(3000); //the server object listens on port 8080
 
