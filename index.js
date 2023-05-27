19b85f288b135b965c10e55acafc2e57c55b48ee
Rozhin <79437690+Rozhin81@users.noreply.github.com>
2023-05-27 17:29:57 +0330

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.write('Hello World!'); //write a response to the client
   res.end(); //end the response
-}).listen(80); //the server object listens on port 8080
+}).listen(3000); //the server object listens on port 8080
 
 main().catch((err) => console.log(err));
 
