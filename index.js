77dd333e181613ad98dbc619b166631506261f59
Rozhin <79437690+Rozhin81@users.noreply.github.com>
2023-05-27 17:45:21 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // getting-started.js
 const mongoose = require("mongoose");
 var http = require('http');
+var info;
 
 main().catch((err) => console.log(err));
 
@@ -15,17 +16,11 @@ async function main() {
       }
     );
     
-
-
-//create a server object:
-http.createServer(function (req, res) {
-  
     var admin = new mongoose.mongo.Admin(mongoose.connection.db);
-    var info = await admin.buildInfo();
-  
-  res.write('Hello World!-----' + info.version); //write a response to the client
-  res.end(); //end the response
-}).listen(3000); //the server object listens on port 8080
-    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+    info = await admin.buildInfo();
 }
 
+http.createServer(function (req, res) {
+  res.write(info);
+  res.end(); 
+}).listen(3000);
